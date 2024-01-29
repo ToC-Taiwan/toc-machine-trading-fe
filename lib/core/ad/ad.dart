d187efa753b927dc6fefc58b509b9c155c1b811a
Tim Hsu@M2MAX-20230327 <maochindada@gmail.com>
2024-01-29 18:25:34 +0800

    feat: integrate banner ads into NewsPage widget
    - Make `bannerAdUnitID` private in the `AD` class
    - Import `google_mobile_ads` and `ad.dart` to `news.dart`
    - Add banner ad implementation to the `NewsPage` widget in `news.dart`
    - Update the app version in `pubspec.yaml` from `4.2.6+40051` to `4.2.7+40052`

--- lib/core/ad/ad.dart
+++ lib/core/ad/ad.dart
@@ -1,6 +1,6 @@
 import 'dart:io' show Platform;
 
 abstract class AD {
-  static final String bannerAdUnitID = Platform.isIOS ? 'ca-app-pub-1617900048851450/8822922940' : 'ca-app-pub-1617900048851450/4933705826';
-  static String get bannerAdUnitId => bannerAdUnitID;
+  static final String _bannerAdUnitID = Platform.isIOS ? 'ca-app-pub-1617900048851450/8822922940' : 'ca-app-pub-1617900048851450/4933705826';
+  static String get bannerAdUnitId => _bannerAdUnitID;
 }
--- lib/features/news/pages/news.dart
+++ lib/features/news/pages/news.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:toc_machine_trading_fe/core/ad/ad.dart';
 import 'package:toc_machine_trading_fe/core/api/twse.dart';
 import 'package:toc_machine_trading_fe/features/universal/widgets/app_bar.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -13,7 +15,20 @@ class NewsPage extends StatefulWidget {
 }
 
 class _NewsPageState extends State<NewsPage> {
+  late Orientation _currentOrientation;
+
   Future<List<TWSENews>> newsList = TWSE.getTWSENews();
+
+  bool _isLoaded = false;
+  BannerAd? _inlineAdaptiveAd;
+  AdSize? _adSize;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _currentOrientation = MediaQuery.of(context).orientation;
+    _loadAd();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,6 +43,9 @@ class _NewsPageState extends State<NewsPage> {
             if (snapshot.hasData) {
               return ListView.separated(
                 separatorBuilder: (context, index) {
+                  if (index == 5) {
+                    return _getAdUnit();
+                  }
                   return Divider(
                     color: Colors.blueGrey[200],
                     thickness: 3,
@@ -79,5 +97,70 @@ class _NewsPageState extends State<NewsPage> {
             );
           },
         ));
+  }
+
+  void _loadAd() async {
+    await _inlineAdaptiveAd?.dispose();
+    setState(() {
+      _inlineAdaptiveAd = null;
+      _isLoaded = false;
+    });
+
+    AdSize size = AdSize.getCurrentOrientationInlineAdaptiveBannerAdSize(
+      _adWidth().truncate(),
+    );
+
+    _inlineAdaptiveAd = BannerAd(
+      adUnitId: AD.bannerAdUnitId,
+      size: size,
+      request: const AdRequest(),
+      listener: BannerAdListener(
+        onAdLoaded: (Ad ad) async {
+          BannerAd bannerAd = (ad as BannerAd);
+          final AdSize? size = await bannerAd.getPlatformAdSize();
+          if (size == null) {
+            return;
+          }
+
+          setState(() {
+            _inlineAdaptiveAd = bannerAd;
+            _isLoaded = true;
+            _adSize = size;
+          });
+        },
+        onAdFailedToLoad: (Ad ad, LoadAdError error) {
+          ad.dispose();
+        },
+      ),
+    );
+    await _inlineAdaptiveAd!.load();
+  }
+
+  double _adWidth() {
+    return MediaQuery.of(context).size.width - (20);
+  }
+
+  Widget _getAdUnit() {
+    return OrientationBuilder(
+      builder: (context, orientation) {
+        if (_currentOrientation == orientation && _inlineAdaptiveAd != null && _isLoaded && _adSize != null) {
+          return Align(
+            child: SizedBox(
+              width: _adWidth(),
+              height: _adSize!.height.toDouble(),
+              child: AdWidget(
+                ad: _inlineAdaptiveAd!,
+              ),
+            ),
+          );
+        }
+        // Reload the ad if the orientation changes.
+        if (_currentOrientation != orientation) {
+          _currentOrientation = orientation;
+          _loadAd();
+        }
+        return Container();
+      },
+    );
   }
 }
