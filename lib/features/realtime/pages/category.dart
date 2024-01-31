d83ec5d709bf0ef16401be5bccc92f6ff03ff16e
Tim Hsu@M2MAX-20230327 <maochindada@gmail.com>
2024-01-31 10:40:35 +0800

    refactor: improve app localization and maintainability, update version
    - Added a new localization string `mxf` in the English, Japanese, Korean, and both forms of Chinese language files.
    - Modified the `RealTimeCategoryPage` class in `category.dart` file to improve readability and maintainability. This included breaking down large chunks of code into smaller, reusable functions like `_buildCustomButtom` and `_buildSheet`.
    - Updated the app version from `4.2.11+40056` to `4.2.12+40057` in the `pubspec.yaml` file.

--- lib/features/realtime/pages/category.dart
+++ lib/features/realtime/pages/category.dart
@@ -19,207 +19,115 @@ class _RealTimeCategoryPageState extends State<RealTimeCategoryPage> {
         context,
         AppLocalizations.of(context)!.realtime,
       ),
-      body: DraggableScrollableSheet(
-        maxChildSize: 0.2,
-        minChildSize: 0.1,
-        initialChildSize: 0.2,
-        snap: true,
-        snapSizes: const [0.2],
-        builder: (context, scrollController) {
-          return LayoutBuilder(
-            builder: (BuildContext context, BoxConstraints constraints) {
-              final double minHeight = MediaQuery.of(context).size.height * 0.1;
-              if (constraints.maxHeight > minHeight) {
-                return Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: const BorderRadius.only(
-                      topLeft: Radius.circular(16),
-                      topRight: Radius.circular(16),
-                    ),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.8),
-                        spreadRadius: 0,
-                        blurRadius: 15,
-                        offset: const Offset(0, -5),
-                      ),
+      body: Stack(
+        children: [
+          DraggableScrollableSheet(
+            maxChildSize: 0.2,
+            minChildSize: 0.1,
+            initialChildSize: 0.2,
+            snap: true,
+            snapSizes: const [0.2],
+            builder: (context, scrollController) {
+              return LayoutBuilder(
+                builder: (BuildContext context, BoxConstraints constraints) {
+                  final double minHeight = MediaQuery.of(context).size.height * 0.1;
+                  if (constraints.maxHeight < minHeight) {
+                    return _buildSheet(context, scrollController);
+                  }
+                  return _buildSheet(
+                    context,
+                    scrollController,
+                    items: [
+                      _buildCustomButtom(AppLocalizations.of(context)!.mxf, Colors.blue[600], Icons.chrome_reader_mode_rounded, onTap: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(fullscreenDialog: true, builder: (context) => FutureRealTimePage(code: AppLocalizations.of(context)!.mxf)),
+                        );
+                      }),
+                      _buildCustomButtom(AppLocalizations.of(context)!.pick_stock, Colors.red[600], Icons.playlist_add_check_circle, onTap: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(fullscreenDialog: true, builder: (context) => const PickStockPage()),
+                        );
+                      }),
+                      _buildCustomButtom('-', Colors.yellow[600], Icons.shopping_cart),
+                      _buildCustomButtom('-', Colors.green[600], Icons.add_home_work_outlined),
                     ],
-                  ),
-                  child: ListView(
-                    controller: scrollController,
-                    physics: const ClampingScrollPhysics(),
-                    children: [
-                      SizedBox(
-                        height: MediaQuery.of(context).size.height * 0.05,
-                        child: const Icon(
-                          Icons.keyboard_double_arrow_down_rounded,
-                          color: Colors.grey,
-                        ),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              ClipOval(
-                                child: Container(
-                                  color: Colors.blue[600],
-                                  height: 55,
-                                  width: 55,
-                                  child: InkWell(
-                                    child: const Icon(
-                                      Icons.chrome_reader_mode_rounded,
-                                      color: Colors.white,
-                                    ),
-                                    onTap: () {
-                                      Navigator.of(context).push(
-                                        MaterialPageRoute(
-                                          fullscreenDialog: true,
-                                          builder: (context) => const FutureRealTimePage(
-                                            code: 'MXF',
-                                          ),
-                                        ),
-                                      );
-                                    },
-                                  ),
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(top: 5),
-                                child: Text(
-                                  'MXF',
-                                  style: Theme.of(context).textTheme.labelLarge,
-                                ),
-                              ),
-                            ],
-                          ),
-                          Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              ClipOval(
-                                child: Container(
-                                  color: Colors.red[600],
-                                  height: 55,
-                                  width: 55,
-                                  child: InkWell(
-                                    child: const Icon(
-                                      Icons.playlist_add_check_circle,
-                                      color: Colors.white,
-                                    ),
-                                    onTap: () {
-                                      Navigator.of(context).push(
-                                        MaterialPageRoute(
-                                          fullscreenDialog: true,
-                                          builder: (context) => const PickStockPage(),
-                                        ),
-                                      );
-                                    },
-                                  ),
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(top: 5),
-                                child: Text(
-                                  AppLocalizations.of(context)!.pick_stock,
-                                  style: Theme.of(context).textTheme.labelLarge,
-                                ),
-                              ),
-                            ],
-                          ),
-                          Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              ClipOval(
-                                child: Container(
-                                  color: Colors.yellow[600],
-                                  height: 55,
-                                  width: 55,
-                                  child: InkWell(
-                                    child: const Icon(
-                                      Icons.shopping_cart,
-                                      color: Colors.white,
-                                    ),
-                                    onTap: () {},
-                                  ),
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(top: 5),
-                                child: Text(
-                                  '-',
-                                  style: Theme.of(context).textTheme.labelLarge,
-                                ),
-                              ),
-                            ],
-                          ),
-                          Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              ClipOval(
-                                child: Container(
-                                  color: Colors.green[600],
-                                  height: 55,
-                                  width: 55,
-                                  child: InkWell(
-                                    child: const Icon(
-                                      Icons.add_home_work_outlined,
-                                      color: Colors.white,
-                                    ),
-                                    onTap: () {},
-                                  ),
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(top: 5),
-                                child: Text(
-                                  '-',
-                                  style: Theme.of(context).textTheme.labelLarge,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                );
-              } else {
-                return Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: const BorderRadius.only(
-                      topLeft: Radius.circular(16),
-                      topRight: Radius.circular(16),
-                    ),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.8),
-                        spreadRadius: 0,
-                        blurRadius: 15,
-                        offset: const Offset(0, -5),
-                      ),
-                    ],
-                  ),
-                  child: ListView(
-                    controller: scrollController,
-                    physics: const ClampingScrollPhysics(),
-                    children: [
-                      SizedBox(
-                        height: MediaQuery.of(context).size.height * 0.05,
-                        child: const Icon(
-                          Icons.keyboard_double_arrow_up_sharp,
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ],
-                  ),
-                );
-              }
+                  );
+                },
+              );
             },
-          );
-        },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Column _buildCustomButtom(String label, Color? color, IconData icon, {void Function()? onTap}) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ClipOval(
+          child: Container(
+            color: color,
+            height: 55,
+            width: 55,
+            child: InkWell(
+              onTap: onTap,
+              child: Icon(
+                icon,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 5),
+          child: Text(
+            label,
+            style: Theme.of(context).textTheme.labelLarge,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Container _buildSheet(BuildContext context, ScrollController scrollController, {List<Widget>? items}) {
+    IconData icon = Icons.keyboard_double_arrow_up_sharp;
+    if (items != null && items.isNotEmpty) {
+      icon = Icons.keyboard_double_arrow_down_rounded;
+    }
+    List<Widget> listViewItem = [
+      SizedBox(
+        height: MediaQuery.of(context).size.height * 0.05,
+        child: Icon(
+          icon,
+          color: Colors.grey,
+        ),
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: items ?? [],
+      ),
+    ];
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(16),
+          topRight: Radius.circular(16),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.8),
+            spreadRadius: 0,
+            blurRadius: 15,
+            offset: const Offset(0, -5),
+          ),
+        ],
+      ),
+      child: ListView(
+        controller: scrollController,
+        physics: const ClampingScrollPhysics(),
+        children: listViewItem,
       ),
     );
   }
