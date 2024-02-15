534a1702dd3cb1e04c07f2d9881aebd7d958dacb
Tim Hsu@M2MAX-20230327 <maochindada@gmail.com>
2024-02-16 02:11:52 +0800

    feat: refactor StockTradePage with TabBar and actions
    - Convert the `StockTradePageState` class into a `TickerProviderStateMixin` to manage the `TabController`.
    - Initialize and dispose the `TabController` in the `initState` and `dispose` methods respectively.
    - Implement a `TabBar` in the AppBar and a `TabBarView` with two tabs in the body of the `StockTradePage`.
    - Replace the static `Stack` widget with a dynamic `TabBarView` in the `StockTradePage`.
    - Add `ElevatedButton` widgets for 'buy' and 'sell' actions in the `TabBarView`.
    - Remove the `NeverScrollableScrollPhysics` from the `TabBar` in `PickStockCategoryPage`.
    - Add `NeverScrollableScrollPhysics` to the `TabBarView` in `PickStockCategoryPage`.

--- lib/features/order/pages/stock.dart
+++ lib/features/order/pages/stock.dart
@@ -9,7 +9,21 @@ class StockTradePage extends StatefulWidget {
   State<StockTradePage> createState() => _StockTradePageState();
 }
 
-class _StockTradePageState extends State<StockTradePage> {
+class _StockTradePageState extends State<StockTradePage> with TickerProviderStateMixin {
+  late final TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,30 +32,95 @@ class _StockTradePageState extends State<StockTradePage> {
         '${AppLocalizations.of(context)!.order}(${AppLocalizations.of(context)!.stock})',
         automaticallyImplyLeading: true,
         disableActions: true,
+        bottom: TabBar(
+          indicatorSize: TabBarIndicatorSize.label,
+          indicatorColor: Colors.blueGrey,
+          overlayColor: MaterialStateProperty.all(Colors.transparent),
+          physics: const NeverScrollableScrollPhysics(),
+          controller: _tabController,
+          tabs: <Widget>[
+            Tab(
+              child: Text(
+                AppLocalizations.of(context)!.lot,
+                style: Theme.of(context).textTheme.titleMedium!,
+              ),
+            ),
+            Tab(
+              child: Text(
+                AppLocalizations.of(context)!.odd,
+                style: Theme.of(context).textTheme.titleMedium!,
+              ),
+            ),
+          ],
+        ),
       ),
       body: SafeArea(
-        child: Stack(
-          children: [
+        child: TabBarView(
+          physics: const NeverScrollableScrollPhysics(),
+          controller: _tabController,
+          children: <Widget>[
             SizedBox(
-              width: double.infinity,
-              height: double.infinity,
-              child: Container(
-                margin: const EdgeInsets.all(20),
-                child: const Stack(
-                  children: [
-                    SizedBox(
-                      width: double.infinity,
-                      height: double.infinity,
+              child: Column(
+                children: [
+                  const Expanded(
+                    flex: 7,
+                    child: Center(
                       child: Icon(
                         Icons.build,
                         size: 100,
                         color: Colors.blueGrey,
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 10, right: 10),
+                      child: SizedBox(
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(backgroundColor: Colors.redAccent),
+                                onPressed: () {},
+                                child: Text(
+                                  AppLocalizations.of(context)!.buy,
+                                  style: const TextStyle(color: Colors.black),
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(backgroundColor: Colors.greenAccent),
+                                onPressed: () {},
+                                child: Text(
+                                  AppLocalizations.of(context)!.sell,
+                                  style: const TextStyle(color: Colors.black),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            )
+            ),
+            const SizedBox(
+              child: Column(
+                children: [
+                  Expanded(
+                    child: Center(
+                      child: Icon(
+                        Icons.build,
+                        size: 100,
+                        color: Colors.deepOrange,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
